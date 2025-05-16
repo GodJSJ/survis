07df35b21adfe630803b0fa2f16d17105ebd84b6
GodJSJ <jsjmlw2024@163.com>
2025-05-16 04:09:50 +0100

    Add files via upload

---
+++ src/data/generated/bib.js
@@ -0,0 +1,105 @@
+﻿const generatedBibEntries = {
+    "Jiang2024Survey": {
+        "author": "Jiang, Juyong and Wang, Fan and Shen, Jiasi and Kim, Sungju and Kim, Sunghun",
+        "doi": "10.48550/arXiv.2406.00515",
+        "journal": "arXiv preprint arXiv:2406.00515",
+        "keywords": "type:survey, large_language_models, code_generation",
+        "title": "A Survey on Large Language Models for Code Generation",
+        "type": "article",
+        "url": "https://arxiv.org/abs/2406.00515",
+        "year": "2024"
+    },
+    "Li2022AlphaCode": {
+        "author": "Li, Yujia and Choi, David and Chung, Junyoung and Kushman, Nate and Schrittwieser, Julian and Leblond, R\u00e9mi and others",
+        "doi": "10.1126/science.abq1158",
+        "journal": "Science",
+        "keywords": "type:system, alpha_code, code_generation",
+        "number": "6624",
+        "pages": "1092--1097",
+        "title": "Competition-level Code Generation with AlphaCode",
+        "type": "article",
+        "url": "https://www.science.org/doi/10.1126/science.abq1158",
+        "volume": "378",
+        "year": "2022"
+    },
+    "Liang2023CodeAsPolicies": {
+        "author": "Liang, Jacky and Huang, Wenlong and Xia, Fei and Xu, Peng and Hausman, Karol and Ichter, Brian and Florence, Pete and Zeng, Andy",
+        "booktitle": "2023 IEEE International Conference on Robotics and Automation (ICRA)",
+        "doi": "10.48550/arXiv.2209.07753",
+        "keywords": "type:robotics, policy_code, code_generation, embodied_control",
+        "title": "Code as Policies: Language Model Programs for Embodied Control",
+        "type": "inproceedings",
+        "url": "https://arxiv.org/abs/2209.07753",
+        "year": "2023"
+    },
+    "Lin2024FlowGen": {
+        "author": "Lin, Feng and Kim, Dong Jae and Chen, Tse-Hsun",
+        "doi": "10.48550/arXiv.2403.15852",
+        "journal": "arXiv preprint arXiv:2403.15852",
+        "keywords": "type:agent, process_model, software_engineering",
+        "title": "SOEN-101: Code Generation by Emulating Software Process Models Using Large Language Model Agents (FlowGen)",
+        "type": "article",
+        "url": "https://arxiv.org/abs/2403.15852",
+        "year": "2024"
+    },
+    "Liu2024ClassEval": {
+        "author": "Liu, Mingwei and Du, Xueying and Li, Junwei and Wang, Hanlin and Peng, Xin and Lou, Yiling",
+        "booktitle": "Proceedings of the 46th International Conference on Software Engineering (ICSE '24)",
+        "doi": "10.1145/3597503.3639219",
+        "keywords": "type:evaluation, class_level, code_generation",
+        "title": "Evaluating Large Language Models in Class-Level Code Generation",
+        "type": "inproceedings",
+        "url": "https://doi.org/10.1145/3597503.3639219",
+        "year": "2024"
+    },
+    "Mahamud2023CodeQuality": {
+        "author": "Mahamud, Mosleh and Samsten, Isak",
+        "doi": "10.48550/arXiv.2309.09264",
+        "journal": "arXiv preprint arXiv:2309.09264",
+        "keywords": "type:evaluation, code_quality, transformers",
+        "title": "Code Quality Assessment Using Transformers",
+        "type": "article",
+        "url": "https://arxiv.org/abs/2309.09264",
+        "year": "2023"
+    },
+    "Ridnik2024AlphaCodium": {
+        "author": "Ridnik, Tal and Kredo, Dedy and Friedman, Itamar",
+        "doi": "10.48550/arXiv.2401.08500",
+        "journal": "arXiv preprint arXiv:2401.08500",
+        "keywords": "type:framework, alphacodium, flow_engineering, code_generation",
+        "title": "Code Generation with AlphaCodium: From Prompt Engineering to Flow Engineering",
+        "type": "article",
+        "url": "https://arxiv.org/abs/2401.08500",
+        "year": "2024"
+    },
+    "Xu2024ChainOfProgramming": {
+        "author": "Xu, Shuaicheng and Hou, Shuyang and Jiao, Haoyue and Shen, Zhangxiao and Liang, Jianyuan and Zhao, Anqi and Zhang, Xiaopu and Wu, Huayi",
+        "doi": "10.48550/arXiv.2411.10753",
+        "journal": "arXiv preprint arXiv:2411.10753",
+        "keywords": "type:application, chain_of_programming, geospatial, code_generation",
+        "title": "Chain-of-Programming (CoP): Empowering Large Language Models for Geospatial Code Generation",
+        "type": "article",
+        "url": "https://arxiv.org/abs/2411.10753",
+        "year": "2024"
+    },
+    "Ye2024SurveyEvalLLMs": {
+        "author": "Ye, Wei and Chen, Liguo and Guo, Qi and Jia, Hongrui and Zeng, Zhengran and Wang, Xin and others",
+        "doi": "10.48550/arXiv.2408.16498",
+        "journal": "Journal of Computer Science and Technology, in press",
+        "keywords": "type:survey, evaluation, llms, code_generation",
+        "title": "A Survey on Evaluating Large Language Models in Code Generation Tasks",
+        "type": "article",
+        "url": "https://arxiv.org/abs/2408.16498",
+        "year": "2024"
+    },
+    "Zheng2024CodeAgent": {
+        "author": "Zheng, Kai and Zhang, Kechi and Li, Jia and Li, Ge and Shi, Xianjie and Jin, Zhi",
+        "booktitle": "Proceedings of ACL 2024 (Volume 1: Long Papers)",
+        "doi": "10.18653/v1/2024.acl-long.737",
+        "keywords": "type:agent, codeagent, repo_level, acl",
+        "title": "CodeAgent: Enhancing Code Generation with Tool-Integrated Agent Systems for Real-World Repo-level Coding Challenges",
+        "type": "inproceedings",
+        "url": "https://aclanthology.org/2024.acl-long.737",
+        "year": "2024"
+    }
+};
